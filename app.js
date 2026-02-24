70aead7cc8071e199d0f45b8769e1b2aceae8a8a
Elder J <johnayo15@gmail.com>
2026-02-24 11:23:40 +0100

    Wrote the function to get computer choice

---
+++ app.js
@@ -0,0 +1,12 @@
+// Function to generate computer choice
+function getComputerChoice() {
+  let randomNumber = Math.floor(Math.random() * 3);
+
+  if (randomNumber === 0) {
+    return "rock";
+  } else if (randomNumber === 1) {
+    return "paper";
+  } else {
+    return "scissors";
+  }
+}
